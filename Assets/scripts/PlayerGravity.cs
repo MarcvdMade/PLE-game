8646ee6743be110dd2731bc7b62692a68a10feee
Marc vd Made <marcvdmade01@gmail.com>
2022-06-09 15:59:09 +0200

    custom gravity and gravity rigidbody

--- Assets/scripts/PlayerGravity.cs
+++ Assets/scripts/PlayerGravity.cs
@@ -167,7 +167,7 @@ public class PlayerGravity : MonoBehaviour
 		}
 
 		// customWorldUp.rotation = Quaternion.LookRotation(transform.forward, contactNormal);
-		transform.rotation = Quaternion.LookRotation(transform.forward, contactNormal);
+		// transform.rotation = Quaternion.LookRotation(transform.forward, contactNormal);
 	}
 
 	void FixedUpdate()
